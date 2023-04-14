9a37ec67707e3595ee17776144c9f15dcc42964d
asadshuvo23 <shuvo@agni.com>
2023-04-14 12:32:16 -0600

    first commit

---
+++ src/main/java/com/comit/addressbook/ComitAddressbookApplication.java
@@ -0,0 +1,13 @@
+package com.comit.addressbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComitAddressbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComitAddressbookApplication.class, args);
+	}
+
+}
